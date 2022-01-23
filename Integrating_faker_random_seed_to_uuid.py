729a2e8f97d1ca716ab8714a6ca4ab721236f21a
Pratik K Sarangi <81045908+psarangi550@users.noreply.github.com>
2022-01-23 20:18:04 +0530

    again Modified Commits

--- Integrating_faker_random_seed_to_uuid.py
+++ Integrating_faker_random_seed_to_uuid.py
@@ -2,7 +2,8 @@ import faker
 import random
 import uuid
 import string
-
+print(uuid.NAMESPACE_DNS)
+print(type(uuid.NAMESPACE_DNS))
 #Integrating the random with the uuid
 # num1=random.seed(3)
 # num2=random.randint(1,1000000000000000000000)
@@ -19,11 +20,11 @@ import string
 fake=faker.Faker()#using the Faker module to create a fake object
 # print(dir(fake))
 num=faker.Faker.seed(8786)
-print(fake.name())
+# print(fake.name())
 # print(num)
-name1=fake.name().split()[0]
-print(type(name1))
-print(name1)
-# print(fake.uuid4())
-# UUID2=uuid.UUID(name1)
+# name1=fake.name().split()[0].rstrip('')
+# print(type(name1))
+# print(name1)
+# # print(fake.uuid4())
+# UUID2=uuid.UUID("hello")
 # print(UUID2)
