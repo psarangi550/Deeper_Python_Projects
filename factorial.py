40b9b4fb1642e92da02d37d13dee60c84fb7ef89
psarangi550 <psarangi50@gmail.com>
2022-02-07 11:04:35 +0530

    All Functools commits

--- factorial.py
+++ factorial.py
@@ -1,16 +1,16 @@
-def fact(n):
-    if n==0:
-        return 1
-    else:
-        return n*fact(n-1)
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*fact(n-1)
 
 
-def invoke_factorial(n):
-    result=[]
-    for i in range(n):
-        result.append(fact(i))
-    return result
+# def invoke_factorial(n):
+#     result=[]
+#     for i in range(n):
+#         result.append(fact(i))
+#     return result
 
-print(invoke_factorial(5))
-print(fact(5))
+# print(invoke_factorial(5))
+# print(fact(5))
         
--- fibonaci_series.py
+++ fibonaci_series.py
@@ -1,15 +1,42 @@
-def fibo(n):
-    if n < 2:
-        return n
-    else:
-         return fibo(n-1)+fibo(n-2)
+# def fibo(n):
+#     if n < 2:
+#         return n
+#     else:
+#          return fibo(n-1)+fibo(n-2)
 
-def invoke_fibo(n):    
-    result=[]
-    for i in range(n):
-        result.append(fibo(i))
-    return result   
-
-print(invoke_fibo(5)) 
-
+# def invoke_fibo(n):    
+#     result=[]
+#     for i in range(n):
+#         result.append(fibo(i))
+#     return result   
 # print(fibo(5))
+# print(invoke_fibo(5)) 
+
+#[0,1,1,2,3,5]
+
+#without recursion
+
+# def  fibo(n):
+#     result=[]
+#     a,b=0,1
+#     while(n>0):        
+#         c=a+b
+#         result.append(a)
+#         a=b
+#         b=c
+#         n=n-1
+#     return result
+            
+
+def  fibo(n):
+    result=[]
+    a,b=0,1
+    while(n>0):        
+        c=a+b
+        result.append(a)
+        a,b=b,c
+        n=n-1
+    return result       
+    
+        
+print(fibo(7))
