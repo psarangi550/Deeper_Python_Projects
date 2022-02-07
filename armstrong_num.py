07be6e899a64261428c0af9166189055468c3c03
psarangi550 <psarangi50@gmail.com>
2022-02-07 12:06:14 +0530

    All Immediate Commits

--- armstrong_num.py
+++ armstrong_num.py
@@ -2,6 +2,13 @@ x=input("Enter a Number")
 
 def arms(x):
     list1=list(x)
-    print(list1)
-    
-arms(153)
+    result=0
+    for i in list1:
+        result=result+int(i)**len(list1)
+    if result==int(x):
+        print("number is an armstrong number")
+    else:
+        print("its not an armstrong number")
+
+arms(x)
+# arms(153)1
